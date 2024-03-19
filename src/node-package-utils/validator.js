83276d969022de2ca5a4929c8c41b471493326c1
jordanrogus <jordanrogus@gmail.com>
2024-03-19 14:04:48 -0600

    Testing packages and SIs

--- src/node-package-utils/validator.js
+++ src/node-package-utils/validator.js
@@ -4,4 +4,12 @@ export const isEmail = function (email) {
     return validator.isEmail(email);
 }
 
+export const isHexColor = function (hexColor) {
+    return validator.isHexColor(hexColor);
+}
+
+export const isIP = function (ip) {
+    return validator.isIP(ip);
+}
+
 // Additional validations could be added
